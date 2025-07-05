03951ea36aa1e583c262a0dc2c84ef1f9e2b99d0
arvin-ferrer <lietlaw97@gmail.com>
2025-07-05 21:42:44 +0800

    1200

--- bersuball.cpp
+++ bersuball.cpp
@@ -12,17 +12,34 @@ typedef unordered_map<int, int> u_map;
 typedef unordered_map<ll, ll> ll_map;
 
 void solve(){
+  int n, m;
+  cin >> n;
+  vi boys(n);
+  for (int &i: boys) cin >> i;
 
+  cin >> m;
+  vi girls(m);
+  int count = 0;
+  for (int &i: girls) cin >> i;
+  sort(boys.begin(), boys.end());
+  sort(girls.begin(), girls.end());
+  for (int i = 0; i < n; i++){
+    for (int j = 0; j < m; j++){
+      if (abs(boys[i]-girls[j]) <= 1){
+        count++;
+        boys[i] = 10000;
+        girls[j] = 10000;
+        break;
+      }
+    }
+  }
+  cout << count << endl;
 }
 
 int main(){
   ios_base::sync_with_stdio(false);
   cin.tie(0);
-  int t;
-  cin >> t;
-  while(t--){
    solve();
-  }
-
+  
 
 }
