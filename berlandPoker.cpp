2210534b2a2cf44cc2c2154010b908b37269e2f6
arvin-ferrer <lietlaw97@gmail.com>
2025-07-16 16:00:37 +0800

    mathhhh fvck

--- berlandPoker.cpp
+++ berlandPoker.cpp
@@ -12,6 +12,20 @@ typedef unordered_map<int, int> u_map;
 typedef unordered_map<ll, ll> ll_map;
 
 void solve(){
+  int n, m, k;
+  cin >> n >> m >> k;
+  int each = n/k;
+  if (each >= m) cout << m << endl;
+  else{
+    int remainPlayer = k-1;
+    int mxJoker = min(each, m); 
+    int remainJoker = m-mxJoker; 
+    double distribute = ceil((double) remainJoker/remainPlayer);
+    //remaining joker will be distributed to remainPlayer
+    cout << mxJoker-distribute<< endl;
+
+
+  }
 
 }
 
