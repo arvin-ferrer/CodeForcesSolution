bc128d688c6ef8f14613bd206baddf725c2557ec
arvin-ferrer <lietlaw97@gmail.com>
2025-07-20 14:51:44 +0800

    .

---
+++ threeIndices.cpp
@@ -0,0 +1,42 @@
+/*
+  Created by: acferrer
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef vector <ll> vll;
+typedef vector<int> vi;
+typedef unordered_map<int, int> u_map;
+typedef unordered_map<ll, ll> ll_map;
+
+void solve(){
+  
+  int n;
+  cin >> n;
+  vi a(n);
+  for (int &i: a) cin >> i;
+  bool good = false;
+  for (int i = 1; i < n-1; i++){
+    if (a[i-1] < a[i] && a[i] > a[i+1]){
+      cout << "YES\n";
+      cout << i << ' ' << i+1 << ' ' << i+2 << endl;
+      good = true;
+      break;
+    }
+  }
+  if (!good) cout << "NO\n";
+}
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  int t;
+  cin >> t;
+  while(t--){
+   solve();
+  }
+
+
+}
