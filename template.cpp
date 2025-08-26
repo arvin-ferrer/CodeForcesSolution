b1f8bb162329fe1fab3e7a5e5082e8a601b05cc7
arvin-ferrer <lietlaw97@gmail.com>
2025-08-26 20:01:27 +0800

    Updated template

--- template.cpp
+++ template.cpp
@@ -1,7 +1,7 @@
 /*
   Created by: acferrer
 */
-// #include <bits/stdc++.h>
+
 #include <iostream>
 #include <set>
 #include <stack>
@@ -18,6 +18,8 @@ typedef vector <ll> vll;
 typedef vector<int> vi;
 typedef unordered_map<int, int> u_map;
 typedef unordered_map<ll, ll> ll_map;
+typedef set<int> si;
+typedef set<ll> sl;
 
 void solve(){
 
